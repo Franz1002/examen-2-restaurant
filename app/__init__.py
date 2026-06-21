37da7031336ee8fe338316fc9702d4842abcf398
Franz1002 <franzerikcoricopaly@gmail.com>
2026-06-21 13:57:03 -0400

    Se corrige conflicto de merge en __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from .extensions import appbuilder, db
-<<<<<<< Updated upstream
-=======
 from .views.categoria_view import CategoriaView
 from .models.categoria import Categoria
 from .views.menu_view import MenuView
 from .models.menu import Menu
 from .views.cliente_view import ClienteView
 from .models.cliente import Cliente
->>>>>>> Stashed changes
 
 
 def create_app() -> Flask:
@@ -19,12 +16,9 @@ def create_app() -> Flask:
         appbuilder.init_app(app, db.session)
         
         db.create_all()
-<<<<<<< Updated upstream
-=======
         
         appbuilder.add_view(CategoriaView, "Categorias", icon="fa-folder-open-o", category="Catálogos")
         appbuilder.add_view(MenuView, "Menus", icon="fa-cutlery", category="Catálogos")
         appbuilder.add_view(ClienteView, "Clientes", icon="fa-user", category="Ventas")
->>>>>>> Stashed changes
      
     return app
